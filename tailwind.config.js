e1f71842afdc019e5a494510298290af4adfb1fe
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2022-04-05 11:13:05 -0500

    Add: testimonial-01 to testimonial-03 sections

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        "dark-violet": "#1E0E62",
+        "dark-violet-2": "#151439",
+        "light-violet": "#482BE7",
+        "green": "#25DAC5",
+        "primary": "#2F1893",
+        "red": "#EA3223",
+        "pink": "#E93A7D",
+        "blue": "#1DA1F2",
+        "grey": "#EBEAED",
+      },
+      fontFamily: {
+        "sans": "DM Sans"
+      },
+      backgroundImage: {
+        form01: 'url("../img/form-01.png")',
+        form04: 'url("../img/form-04.png")',
+        form06: 'url("../img/form-06.png")',
+        form07: 'url("../img/form-07.png")',
+        form08: 'url("../img/form-08.png")',
+        form15: 'url("../img/form-15.png")',
+      },
+    },
+  },
+  plugins: [
+  ],
+}
